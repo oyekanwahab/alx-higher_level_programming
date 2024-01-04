aa275c8c6f98a5ca3dfd37729c58af592b8e8405
oyekan Abdul-wahab <oyekanwahab@gmai.com>
2024-01-04 14:41:58 -0800

    Push for task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-num = number
+num = number % 10
 if num < 0:
     print(f"{num} is negative")
 elif num > 0:
     print(f"{num} is positve")
 else:
     print(f"{num} is zero")
+
